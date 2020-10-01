6ccd4f51fad41455b96a4a3c4630e4155486e5fb
Nowsher Ali Shovon <46531031+nowshershovon@users.noreply.github.com>
2020-10-01 17:23:40 +0600

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,19 @@
+while True:
+    var = input("massage> ")
+    if (var == "hello"):
+        print("Hi")
+
+    var1 = input("massage> ")
+    if (var1 == "how are you"):
+        print("Fine")
+
+    var2 = input("massage> ")
+    if (var2 == "who are You"):
+        print("i am google")
+
+    var3 = input("massage> ")
+    if (var3 == "bye"):
+        print("bye")
+
+    print("EXIT")
+
